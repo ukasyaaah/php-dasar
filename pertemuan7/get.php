2330c657bedd80dd39ff6f3bfd944df5247c6807
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-06-20 06:21:30 +0700

    Get

---
+++ pertemuan7/get.php
@@ -0,0 +1,82 @@
+<?php
+// Variabel Scope / lingkup variabel
+$lokal = 10; // ini adalah variabel lokal yg hanya bisa diakses di file ini
+
+function varFunc()
+{
+    $func = 20; // ini adalah variabel lokal utk function ini aja
+    echo $func;
+}
+
+// agar variabel lokal bisa diakses di function tambahkan 'global'
+function iniVarGlobal()
+{
+    global $lokal; // ini menggunakan var yg ada di luar function
+    echo $lokal;
+}
+
+varFunc(); // 20
+echo '<br>';
+iniVarGlobal(); // 10
+echo '<br>';
+
+// Superglobals : variabel2 global milik php yg bisa diakses dimanapun & kapanpun
+// Dari 7 ini, semuanya adalah array assosiative
+// $_GET -> datanya dikirim lewat url
+// $_POST -> datanya dikirim lewat form (bisa juga dgn get)
+// $_REQUEST
+// $_SESSION
+// $_COOKIE
+// $_SERVER
+// $_ENV
+
+echo $_GET;
+echo '<br>';
+var_dump($_GET);
+
+$hp = [
+    [
+        'nama' => 'x100',
+        'merk' => 'vivo',
+        'warna' => 'hijau',
+        'processor' => 'Snapdragon 8 gen 3',
+        'harga' => 'Rp. 11.000.000',
+    ],
+    [
+        'nama' => 'iPhone 16 Plus',
+        'merk' => 'Apple',
+        'warna' => 'Silver',
+        'processor' => 'A18',
+        'harga' => 'Rp. 21.000.000',
+    ],
+    [
+        'nama' => 'S25',
+        'merk' => 'Samsung',
+        'warna' => 'Black',
+        'processor' => 'Snapdragon 8 gen 3 for Galaxy',
+        'harga' => 'Rp. 29.000.000',
+    ],
+];
+?>
+php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Daftar HanfP</h1>
+    <ul>
+        <?php foreach ($hp as $hape) : ?>
+            <!-- // Data dikirim dengan request method get -->
+            <!-- Metode request get adlh metode pengiriman data melalui url & data tsb bisa diambil / ditangkap oleh variabel super global $_GET-->
+            <li><a href="to_get.php?nama=<?php echo $hape['nama'] ?>&merk=<?php echo $hape['merk'] ?>&warna=<?php echo $hape['warna'] ?>&processor=<?php echo $hape['processor'] ?>&harga=<?php echo $hape['harga'] ?> "><?= $hape['nama'] ?></li></a>
+        <?php endforeach; ?>
+    </ul>
+</body>
+
+</html>
---
+++ pertemuan7/to_get.php
@@ -0,0 +1,42 @@
+<?php
+// Cek apakah ga ada data di $_GET -23.41
+if (
+    // Klo $_GET['nama'] dll blm di set, maka pindah ke halaman latihan1.php
+    !isset($_GET['nama']) ||
+    !isset($_GET['merk']) ||
+    !isset($_GET['warna']) ||
+    !isset($_GET['processor']) ||
+    !isset($_GET['harga'])
+) {
+    // redirect
+    // harus bener, Location: (jgn spasi setelah Location)
+    header("Location:get.php");
+    exit;
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail Hp</title>
+</head>
+
+<body>
+    <h1>Detail HP</h1>
+    <ul>
+        <!-- dan data ditangkap oleh variabel super global $_GET  -->
+        <li>Nama : <?php echo $_GET['nama'] ?></li>
+        <li>Merk : <?php echo $_GET['merk'] ?></li>
+        <li>Warna : <?php echo $_GET['warna'] ?></li>
+        <li>Processor : <?php echo $_GET['processor'] ?></li>
+        <li>Harga : <?php echo $_GET['harga'] ?></li>
+    </ul>
+
+    <a href="get.php">Kembali ke Daftar HP</a>
+</body>
+
+</html>
