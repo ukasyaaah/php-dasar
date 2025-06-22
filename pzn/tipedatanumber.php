f044c08e7711e26b6acdec2cd2731ff1e612f8b5
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-06-22 13:04:00 +0700

    floating point

--- pzn/tipedatanumber.php
+++ pzn/tipedatanumber.php
@@ -9,9 +9,21 @@ var_dump(01234); // 668
 echo 'Hexadecimal :';
 var_dump(0x2f); // 47
 
-
 echo 'Binary :';
 var_dump(0b11111); // 31
 
 echo 'Underscore in number :';
+// untuk mempermudah membaca angka
 var_dump(1_000_000); // 1000000
+
+echo 'Floating Point : ';
+var_dump(1.300);
+
+echo 'Floating Point dg E Notation + : 1.2 x 1000 :';
+var_dump(1.2e3);
+
+echo 'Floating Point dg E Notation - : 1.2 x 0.001 : ';
+var_dump(1.2e-3);
+
+echo 'Underscore di floating point : ';
+var_dump(1_200_000);
