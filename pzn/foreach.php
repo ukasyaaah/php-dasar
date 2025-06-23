9f15484de5bccd4566a1a35261dbd1affb563792
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-06-23 22:24:17 +0700

    foreach

--- pzn/foreach.php
+++ pzn/foreach.php
@@ -10,3 +10,27 @@ foreach ($nama as $n):
     echo "Nama ku $n" . PHP_EOL;
     echo "\n";
 endforeach;
+
+// For each dgn key
+
+$person = [
+    "first" => "Ukhasyah",
+    "last" => "Fauzan",
+    "nilai" => [
+        "mtk" => 70
+    ]
+];
+
+
+foreach ($person as $key => $value) :
+    echo $key . PHP_EOL; // tampilkan key nya
+
+    /// Lakukan Pengecekan, apakah value nya array
+    if (is_array($value)) {
+        foreach ($value as $k => $v) {
+        }
+    }
+    echo $value  . PHP_EOL;
+    echo $v;
+
+endforeach;
